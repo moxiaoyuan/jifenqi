56b0f16bd478c0fc9a7a917ef31cb13d1ad977dc
moxiaoyuan <hqq1@admindeiMac-32.local>
2016-05-09 21:22:39 +0800

    add
    add cheng gong

---
+++ jifenqi/Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging-Header.h
+//  jifenqi
+//
+//  Created by hqq1 on 16/5/9.
+//  Copyright © 2016年 hqq1. All rights reserved.
+//
+
+
+#import "sqlite3.h"
+#import <time.h>
--- jifenqi/jifenqi/ViewController.swift
+++ jifenqi/jifenqi/ViewController.swift
@@ -11,50 +11,54 @@ import UIKit
 class ViewController: UIViewController {
 
     var operand1:String = ""//缓存的字符串
-    var operand2:Int = 0
-    var huancun:Int = 0//缓存
-    var operator1:String = "+"//记录操作符
+    var operand2:String = ""
+    var huancun1 = 0
+    var huancun2 = 0
     
-    @IBOutlet weak var time: UILabel!
+    
     @IBOutlet weak var result_1: UITextField!
     @IBOutlet weak var result_2: UITextField!
     @IBAction func point1(sender: UIButton) {
         let value = sender.currentTitle//提取每次取的值
-        operand1 = operand1 + value!
+        huancun1 = huancun1+Int(value!)!
+        operand1 = String(huancun1)
         result_1.text = operand1
-        operand2 = operand2 +
-        result_2.text = operand2
-            var result = 0
-            huancun=Int(operand1)! + Int(operand2)!
-            result=huancun
-            result_1.text = "\(result)"
         
-        
-
     }
     
     @IBAction func point2(sender: UIButton) {
-        var result = 0
-        result=Int(operand1)! + Int(operand2)!
-        result_1.text = "\(result)"
+        let value = sender.currentTitle//提取每次取的值
+        huancun2 = huancun2+Int(value!)!
+        operand2 = String(huancun2)
+        result_2.text = operand2
     }
     
     @IBAction func bton(sender: AnyObject) {
         let value = sender.currentTitle//提取每次取的值
         if value == "Stop"
         {
-            //result_1.text="\(operator1)"
-            //result_2.text="\(operator1)"
+            
             return
         }
         else if value == "Start"{
             
         }
-        else if value == "Finish"{
-            
+        else if value == "Reset"{
+            operand1 = ""
+            operand2 = ""
+            huancun1 = 0
+            huancun2 = 0
+            result_1.text = "0"
+            result_2.text = "0"
+
         }
 
     }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
